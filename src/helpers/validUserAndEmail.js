4fdc1fe875fca39fa73a275b439c7d4a2dc0befa
ThunderGer <Jaames11@hotmail.com>
2023-02-01 23:22:35 -0600

    cambios pequeños :v

--- src/helpers/validUserAndEmail.js
+++ src/helpers/validUserAndEmail.js
@@ -2,9 +2,8 @@ export const validUserAndEmail = (displayName, email) => {
     const newname = displayName.toLowerCase().split(' ')
     try {
         const searchLastName = newname.map((element, index) => {if(element === email.substr(1, (email.indexOf(email.match(/(\d+)/g)[0])-2))) return index+1 }).filter(numero =>  Number.isInteger(numero)).pop()
-        const letter = (searchLastName === 1)?newname[searchLastName+1]:newname[searchLastName*0]
-        const lastName = (searchLastName === 1)?newname[searchLastName*0]:newname[searchLastName-1]
-        return email.substr(0,email.search('@')).includes(letter.substr(0, 1) + lastName + newname[searchLastName].substr(0, 1))
+        const letter = (searchLastName === 1)?newname[searchLastName+1]:newname[searchLastName*0].substr(0, 1)
+        return email.substr(0,email.search('@')).includes(letter + (searchLastName === 1)?newname[searchLastName*0]:newname[searchLastName-1] + newname[searchLastName].substr(0, 1))
     } catch (error) {
         return false
     }
